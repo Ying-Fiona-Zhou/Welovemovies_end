cb59817ea4d7decae9b536265e5ba918a882be1c
Ying <ying6.zhou@gmail.com>
2025-11-12 15:28:33 -0500

    Fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -12,10 +12,10 @@ module.exports = {
     connection: DATABASE_URL,  // 本地用自己的 PostgreSQL
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: "./src/db/migrations",
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: "./src/db/seeds",
     },
   },
 
@@ -27,10 +27,10 @@ module.exports = {
     },
     pool: { min: 2, max: 10 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: "./src/db/migrations",
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: "./src/db/seeds",
     },
   },
 
